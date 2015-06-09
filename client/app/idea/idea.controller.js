050318f8fa7394b2c632c234386423656b751b9f
MilenkoBugueno <milenko.bugueno@gmail.com>
2015-06-09 19:30:14 +0200

    Create and delete ideas in front end

--- client/app/idea/idea.controller.js
+++ client/app/idea/idea.controller.js
@@ -1,7 +1,28 @@
 'use strict';
 
 angular.module('teamtoolApp')
-  .controller('IdeaCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('IdeaCtrl', function ($scope, $http, socket) {
     $scope.awesomeIdeas = [];
+
+    $http.get('/api/ideas').success(function(awesomeIdeas) {
+      $scope.awesomeIdeas = awesomeIdeas;
+      socket.syncUpdates('idea', $scope.awesomeIdeas);
+    });
+
+    $scope.addIdea = function() {
+      if($scope.newIdea === '') {
+        return;
+      }
+      $http.post('/api/ideas', { name: $scope.newIdea });
+      $scope.newIdea = '';
+    };
+
+    $scope.deleteIdea = function(idea) {
+      $http.delete('/api/ideas/' + idea._id);
+    };
+
+    $scope.$on('$destroy', function () {
+      socket.unsyncUpdates('idea');
+    });
+
   });
